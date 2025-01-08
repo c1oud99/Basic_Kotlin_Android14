e6b10df070a18032fb8b0ab686abccf0925012df
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-08 16:53:57 +0900

    Update LocationViewModel

--- MyShoppingListApp/app/src/main/java/eu/tutorials/myshoppinglistapp/LocationViewModel.kt
+++ MyShoppingListApp/app/src/main/java/eu/tutorials/myshoppinglistapp/LocationViewModel.kt
@@ -1,4 +1,13 @@
 package eu.tutorials.myshoppinglistapp
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+
 class LocationViewModel {
+    private val _location = mutableStateOf<LocationData?>(null)
+    val location : State<LocationData?> = _location
+
+    fun updateLocation(newLocation: LocationData){
+        _location.value = newLocation
+    }
 }
