2ec3d1f51c9e1e88ad5a7a145d9cd05c6bba37ee
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-08 16:50:19 +0900

    Update LocationSelectionScreen.kt

--- MyShoppingListApp/app/src/main/java/eu/tutorials/myshoppinglistapp/LocationSelectionScreen.kt
+++ MyShoppingListApp/app/src/main/java/eu/tutorials/myshoppinglistapp/LocationSelectionScreen.kt
@@ -1,11 +1,21 @@
 package eu.tutorials.myshoppinglistapp
 
 import android.location.Location
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
@@ -19,6 +29,27 @@ fun LocationSelectionScreen(
 
     var cameraPositionState = rememberCameraPositionState{
         position = CameraPosition.fromLatLngZoom(userLocation.value, 10f)
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        GoogleMap(
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 16.dp)
+            cameraPositionState = cameraPositionState,
+            onMapClick = {
+                userLocation.value = it
+            }
+        ) {
+            Marker(state = MarkerState(position = userLocation.value))
+        }
+
+        Button(onClick = {
+            newLocation = LocationData(userLocation.value.latitude, userLocation.value.longitude)
+            onLOcationSelected("newLocation")
+        }) {
+            Text("Set Location")
+        }
     }
 
 }
