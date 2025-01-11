e07e78913dfa6d085c58197b774d20e39d2cfc84
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-11 15:36:21 +0900

    Update color.xml(add green)

--- MyWishListApp/app/build.gradle.kts
+++ MyWishListApp/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "eu.tutorials.mywishlistapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "eu.tutorials.mywishlistapp"
