a96efb486e9ae63d7665adf2652a93d6804494ea
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-11 18:00:40 +0900

    Update Screen class

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/Screen.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/Screen.kt
@@ -1,4 +1,6 @@
 package eu.tutorials.mywishlistapp
 
-class Screen {
+sealed class Screen(val route:String) {
+    object HomeScreen: Screen("home_screen")
+    object AddScreen: Screen("add_screen")
 }
