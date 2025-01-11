6b210d2e0b03ae8c49237c1781092d2ff54f5145
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-11 15:41:12 +0900

    Update AppBar.kt

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AppBar.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AppBar.kt
@@ -3,10 +3,15 @@ package eu.tutorials.mywishlistapp
 import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -15,6 +20,17 @@ fun AppBarView(
     title: String,
     onBackNavClicked: () -> Unit= {}
 ){
+    val navigationIcon : (@Composable () -> Unit)? =
+        {
+            IconButton(onClick = { onBackNavClicked() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    tint = Color.White,
+                    contentDescription = null
+                )
+            }
+        }
+
     TopAppBar(
         title = {
             Text(text = title,
@@ -24,6 +40,7 @@ fun AppBarView(
                     .heightIn(max = 24.dp))
         },
         elevation = 3.dp,
-        backgroundColor = colorResource(id = R.color.app_bar_color)
+        backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
     )
 }
