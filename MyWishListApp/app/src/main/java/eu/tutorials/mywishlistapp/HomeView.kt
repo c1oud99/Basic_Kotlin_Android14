2f06c5ff3e6a02456dc490412943139753eab566
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-11 17:18:42 +0900

    Update Wish data class

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
@@ -1,12 +1,15 @@
 package eu.tutorials.mywishlistapp
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -15,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import eu.tutorials.mywishlistapp.data.Wish
 
 @Composable
 fun HomeView(){
@@ -51,5 +55,9 @@ fun HomeView(){
 
 @Composable
 fun WishItem(wish: Wish, onClick: () -> Unit){
+    Card(modifier = Modifier.fillMaxWidth().padding(top = 8.pd, start = 8.dp, end = 8.dp).clickable {
 
+    }) {
+
+    }
 }
