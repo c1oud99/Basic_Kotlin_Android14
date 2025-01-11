5b658c2d5f9d3700d65e2c77d26201d55994e60b
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-11 23:24:49 +0900

    Update AddEditDetaiView.kt

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
@@ -1,2 +1,20 @@
 package eu.tutorials.mywishlistapp
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AddEditDetailView(
+    id: Long,
+    viewModel: WishViewModel,
+    navController: NavController
+){
+  Scaffold(
+      topBar = { AppBarView(title =
+      if(id != 0L) "Update Wish" else "Add Wish"
+      )}
+  ){
+
+  }
+}
