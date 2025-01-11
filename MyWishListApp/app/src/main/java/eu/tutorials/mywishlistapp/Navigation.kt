6a595a56ec84f78f5ddc775fd45882b3e5f4d982
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-11 20:13:22 +0900

    Update Navigation.kt

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/Navigation.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/Navigation.kt
@@ -1,2 +1,8 @@
 package eu.tutorials.mywishlistapp
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation(viewModel: WishViewModel = viewModel(), navController: NavController = rememberNavController())
