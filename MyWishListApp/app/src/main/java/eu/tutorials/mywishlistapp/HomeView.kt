dd364cbf3743cb515936891e8de9845c5f48bdf3
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-11 16:42:44 +0900

    Update HomeView.kt

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
@@ -29,7 +29,8 @@ fun HomeView(){
                 modifier = Modifier.padding(all = 20.dp),
                 contentColor = Color.White,
                 backgroundColor = Color.Black,
-                onClick = { // TODO Add Navigation to add screen
+                onClick = {
+                    Toast.makeText(context, "FAButton Clicked", Toast.LENGTH_LONG).show()
                 }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
