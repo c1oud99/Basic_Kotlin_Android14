f729fa99339ba6eea7c51348232c06818b062b01
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-12 15:49:25 +0900

    Update AddEditDetaiView.kt

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
@@ -8,17 +8,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -68,9 +71,16 @@ fun WishTextField(
             focusedBorderColor = colorResource(id = R.color.black),
             unfocusedBorderColor = colorResource(id = R.color.black),
             cursorColor = colorResource(id = R.color.black),
-            focusedLabelColor = colorResource(id = R.color.black)
-            unfocusedLableColor = colorResource(id = R.color.black)
+            focusedLabelColor = colorResource(id = R.color.black),
+            unfocusedLabelColor = colorResource(id = R.color.black)
 
         )
     )
+}
+
+
+@Preview
+@Composable
+fun WishTestFieldPrev(){
+    WishTextField(label = "text", value = "text", onValueChanged = {})
 }
