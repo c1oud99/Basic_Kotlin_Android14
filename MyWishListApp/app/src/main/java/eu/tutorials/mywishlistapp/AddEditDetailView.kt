dd15f9e91393b03437ede8537c1b321d2f0ae6c9
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-12 14:44:50 +0900

    Update AddEditDetaiView.kt

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
@@ -3,14 +3,22 @@ package eu.tutorials.mywishlistapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -21,10 +29,9 @@ fun AddEditDetailView(
     navController: NavController
 ){
 
-  Scaffold(
-      topBar = { AppBarView(title =
+  Scaffold(topBar = {AppBarView(title =
       if(id != 0L) stringResource(id = R.string.update_wish) "Update Wish"
-          else strinngResource(id = R.string.add_wish) "Add Wish"
+      else stringResource(id = R.string.add_wish) "Add Wish"
       )}) { 
         Column(modifier = Modifier
             .padding(it)
@@ -33,6 +40,37 @@ fun AddEditDetailView(
             verticalArrangement = Arrangement.Center
         ) {
           Spacer(modifier = Modifier.height(10.dp))
+
+
+
+
         }
   }
+}
+
+
+@Composable
+fun WishTextField(
+    label: String,
+    value: String,
+    onValueChanged: (String) -> Unit
+){
+    OutlinedTextField(
+        value = value, 
+        onValueChange = onValueChanged,
+        label = { Text(text = label, color = Color.Black)},
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            // using predefined Color
+            textColor = Color.Black,
+            // using our own colors in Res.Values.Color
+            focusedBorderColor = colorResource(id = R.color.black),
+            unfocusedBorderColor = colorResource(id = R.color.black),
+            cursorColor = colorResource(id = R.color.black),
+            focusedLabelColor = colorResource(id = R.color.black)
+            unfocusedLableColor = colorResource(id = R.color.black)
+
+        )
+    )
 }
