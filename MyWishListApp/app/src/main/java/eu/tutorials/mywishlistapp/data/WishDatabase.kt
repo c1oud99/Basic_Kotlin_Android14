11b4a06b2eca290a55eb31fff512ca5c53311336
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-13 17:22:30 +0900

    Update WishDatabase class

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/data/WishDatabase.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/data/WishDatabase.kt
@@ -1,4 +1,12 @@
 package eu.tutorials.mywishlistapp.data
 
-class WishDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+@Database(
+    entities = [Wish::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class WishDatabase : RoomDatabase() {
+    abstract fun wishDao(): WishDao
 }
