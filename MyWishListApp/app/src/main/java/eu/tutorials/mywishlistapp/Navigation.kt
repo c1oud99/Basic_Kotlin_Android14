76c84e2a81d24a1438c15c286f4874fda02cac64
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-15 16:25:26 +0900

    Update Navigation

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/Navigation.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/Navigation.kt
@@ -4,9 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 @Composable
 fun Navigation(viewModel: WishViewModel = viewModel(),
@@ -19,7 +21,16 @@ fun Navigation(viewModel: WishViewModel = viewModel(),
             HomeView(navController, viewModel)
         }
 
-        composable(Screen.HomeScreen.route){
+        composable(Screen.HomeScreen.route + "/{id}",
+            arguments = listOf(
+                navArgument("id"){
+                    type = NavType.LongType
+                    defaultValue = 0L
+                    nullable = false
+                }
+            )
+        ){ entry->
+            val id = if(entry.arguments != null) entry.arguments!!.getLong("id") else 0L
             AddEditDetailView(id = 0L, viewModel = viewModel, navController = navController)
         }
     }
