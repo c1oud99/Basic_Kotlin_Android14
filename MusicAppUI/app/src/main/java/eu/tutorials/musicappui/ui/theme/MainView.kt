f2bbda305163468925657398ddf11bb12f0a8aed
c1oud99 <1imepie.c1oud9@gmail.com>
2025-01-16 14:43:34 +0900

    Update ui.theme/MainView.kt

--- MusicAppUI/app/src/main/java/eu/tutorials/musicappui/ui/theme/MainView.kt
+++ MusicAppUI/app/src/main/java/eu/tutorials/musicappui/ui/theme/MainView.kt
@@ -1,6 +1,11 @@
 package eu.tutorials.musicappui.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -8,12 +13,17 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import eu.tutorials.musicappui.Screen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -37,6 +47,11 @@ fun MainView(){
                     Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
                 }}
             )
+        },scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)){
+                items(screenInDrawer)
+            }
         }
 
 
@@ -49,7 +64,26 @@ fun MainView(){
 @Composable
 fun DrawerItem(
     selected: Boolean,
+    item: Screen.DrawerScreen,
     onDrawerItemClicked: () -> Unit
 ){
-
+    val background = if(selected) Color.DarkGray else Color.White
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
+            .clickable {
+                onDrawerItemClicked()
+            }){
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp)
+        )
+        Text(
+            text = item.dTitle,
+            style = MaterialTheme.typography.h5,
+        )
+    }
 }
